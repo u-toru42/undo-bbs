bfde2961439fb3320fa502203cec1ac3f6e49a49
u-toru42 <golt7040@gmail.com>
2019-07-07 19:18:29 +0900

    done

---
+++ app/tests/Fixture/QuestionsFixture(shakyo.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * QuestionsFixture
+ */
+class QuestionsFixture extends TestFixture
+{
+  /**
+   * テーブルのフィールド情報
+   * 
+   * @var array
+   */
+  //@codingStandardsIgnoreStart
+  public $fields = [
+    'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+    'user_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+    'body' => ['type' => 'string', 'length' => 255, 'null'=> false, 'default' => null, 'collate' => 'utf8mb4_general_ci
+    
+    ']
+  ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
