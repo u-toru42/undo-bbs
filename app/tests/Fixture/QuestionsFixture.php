b31838436521c49755e915a349dfcb7e752f30a2
u-toru42 <golt7040@gmail.com>
2019-07-15 21:08:35 +0900

    done

--- app/tests/Fixture/QuestionsFixture.php
+++ app/tests/Fixture/QuestionsFixture.php
@@ -8,49 +8,71 @@ use Cake\TestSuite\Fixture\TestFixture;
  */
 class QuestionsFixture extends TestFixture
 {
-    /**
-     * テーブルのフィールド情報
-     *
-     * @var array
-     */
-    // @codingStandardsIgnoreStart
-    public $fields = [
-        'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
-        'user_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-        'body' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        '_constraints' => [
-            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
-        ],
-        '_options' => [
-            'engine' => 'InnoDB',
-            'collation' => 'utf8mb4_general_ci'
-        ],
-    ];
-    // @codingStandardsIgnoreEnd
+  /**
+   * テーブルのフィールド情報
+   * 
+   * @var array
+   */
+  //@codingStandardsIgnoreStart
+  public $fields = [
+    'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+    'user_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+    'body' => ['type' => 'string', 'length' => 255, 'null'=> false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+    'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+    'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+    '_constraints' => [
+      'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+    ],
+    '_options' => [
+      'engine' => 'InnoDB',
+      'collation' => 'utf8mb4_genral_ci'
+    ],
+  ];
+  // @codingStandardsIgnoreEnd
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
-    {
-        $this->records = [
-            [
-                'id' => 1,
-                'user_id' => 1,
-                'body' => '好きな飲み物は何ですか？',
-                'created' => '2018-12-01 13:00:00',
-                'modified' => '2018-12-01 13:00:00',
-            ],
-            [
-                'id' => 2,
-                'user_id' => 2,
-                'body' => '最近した軽い運動は何ですか？',
-                'created' => '2018-12-02 14:00:00',
-                'modified' => '2018-12-02 14:00:00',
-            ],
-        ];
-        parent::init();
-    }
+  /**
+   * @inheritdoc
+   */
+  public function init()
+  {
+    $this->records = [
+      [
+        'id' => 1,
+        'user_id' => 1,
+        'body' => '室内でもできる運動ってありますか?',
+        'created' => '2019-07-01 00:00:00',
+        'modified' => '2019-07-01 00:00:00',
+      ],
+      [
+        'id' => 2,
+        'user_id' => 2,
+        'body' => '姿勢がピンと伸びるようにしたいです',
+        'created' => '2019-07-02 00:00:00',
+        'modified' => '2019-07-02 00:00:00',
+      ],
+    ];
+    parent::init();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
