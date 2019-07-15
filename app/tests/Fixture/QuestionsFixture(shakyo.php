e2b60aefd0279963d7ac711b0f825238c5fcc054
u-toru42 <golt7040@gmail.com>
2019-07-15 21:00:28 +0900

    done

--- app/tests/Fixture/QuestionsFixture(shakyo.php
+++ app/tests/Fixture/QuestionsFixture(shakyo.php
@@ -17,10 +17,42 @@ class QuestionsFixture extends TestFixture
   public $fields = [
     'id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
     'user_id' => ['type' => 'integer', 'length' => 10, 'unsigned' => true, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
-    'body' => ['type' => 'string', 'length' => 255, 'null'=> false, 'default' => null, 'collate' => 'utf8mb4_general_ci
-    
-    ']
-  ]
+    'body' => ['type' => 'string', 'length' => 255, 'null'=> false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+    'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+    'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+    '_constraints' => [
+      'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+    ],
+    '_options' => [
+      'engine' => 'InnoDB',
+      'collation' => 'utf8mb4_genral_ci'
+    ],
+  ];
+  // @codingStandardsIgnoreEnd
+
+  /**
+   * @inheritdoc
+   */
+  public function init()
+  {
+    $this->records = [
+      [
+        'id' => 1,
+        'user_id' => 1,
+        'body' => '',
+        'created' => '',
+        'modified' => '',
+      ],
+      [
+        'id' => 2,
+        'user_id' => 2,
+        'body' => '',
+        'created' => '',
+        'modified' => '',
+      ],
+    ];
+    parent::init();
+  }
 
 
 
